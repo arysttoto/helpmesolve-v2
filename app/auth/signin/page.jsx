e0b1d7e73d2cf1abb6597e869ee735e14fa3438f
arysttoto <aryst.toto@gmail.com>
2023-08-06 21:53:14 +0600

    env updated

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -30,7 +30,7 @@ const SigninPage = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[500px] rounded-md bg-primary bg-opacity-5 py-10 px-6 dark:bg-dark sm:p-[60px]">
                 <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
-                  Sign in to your account
+                  Sign in or Sign up to your account
                 </h3>
                 <p className="mb-11 text-center text-base font-medium text-body-color">
                   Login to your account for a faster checkout.
